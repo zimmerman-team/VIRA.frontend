38dc6686309cb1294b9ace62665a20bd0ecc4131
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-11-20 18:09:10 +0100

    feat: add snackbar component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,11 +3,13 @@
 import React from 'react';
 import Providers from 'app/Providers';
 import { MainRoutes } from 'app/Routes';
+import { PositionedSnackbar } from 'app/components/datadisplay/snackbar';
 
 export function App() {
   return (
     <Providers>
       <MainRoutes />
+      <PositionedSnackbar />
     </Providers>
   );
 }
