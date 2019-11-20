96d5fe260974a450bb5ef1281da2dbe0092d85a6
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-11-20 15:34:20 +0100

    chore: made textfields showup again in storybook
    [IN-65]

---
+++ src/app/components/inputs/common/mock.ts
@@ -0,0 +1,20 @@
+import { fade, Theme } from '@material-ui/core/styles';
+import { CSSProperties } from '@material-ui/styles/withStyles';
+import { ProjectPalette } from 'app/theme';
+
+export const getInputGeneralStyle = (theme: Theme): CSSProperties => {
+  return {
+    borderRadius: 2,
+    position: 'relative',
+    backgroundColor: ProjectPalette.grey[50],
+    border: '1px solid transparent',
+    fontSize: 16,
+    padding: '10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    fontFamily: 'Inter',
+    '&:focus': {
+      boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  };
+};
