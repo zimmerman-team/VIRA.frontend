97cbaa53d6ce9ed5d05e7703d51c0cd928bc9c31
Jim Fung <jim@zimmermanzimmerman.nl>
2019-11-27 12:23:18 +0100

    feat: components

---
+++ src/app/modules/common/components/OutcomeCard.tsx
@@ -0,0 +1,24 @@
+import 'styled-components/macro';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+
+export interface OutcomeCardParams {
+  title: string;
+  description: string;
+}
+export const OutcomeCard = (props: OutcomeCardParams) => {
+  return (
+    <Grid item lg={12}>
+      <Card>
+        <CardHeader title={props.title} />
+        <CardContent>
+          <Typography>{props.description}</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
---
+++ src/app/modules/common/components/TitleParams.tsx
@@ -0,0 +1,74 @@
+import 'styled-components/macro';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { ProjectPalette } from 'app/theme';
+
+export interface TitleParams {
+  title: string;
+  id: string;
+  date?: string;
+  note: string;
+  url_note: string;
+  url: string;
+  description: string;
+}
+export const TitleFragment = (props: TitleParams) => {
+  return (
+    <React.Fragment>
+      <Typography
+        css={`
+          && {
+            font-size: 48px;
+            font-weight: 500;
+          }
+        `}
+      >
+        {props.title}
+      </Typography>
+      <Box height="10px" />
+      <Typography
+        css={`
+          && {
+            font-size: 12px;
+            line-height: 1.33;
+            letter-spacing: 2px;
+            color: ${ProjectPalette.common.black};
+          }
+        `}
+      >
+        {props.id}
+      </Typography>
+      <Box height="10px" />
+      <Typography
+        css={`
+          && {
+            font-size: 12px;
+            line-height: 1.33;
+            letter-spacing: 2px;
+            color: ${ProjectPalette.common.black};
+          }
+        `}
+      >
+        {props.note}
+      </Typography>
+      <Box height="10px" />
+      <Typography
+        css={`
+          && {
+            font-size: 12px;
+            line-height: 1.33;
+            letter-spacing: 2px;
+
+            a {
+              text-decoration: none;
+              color: ${ProjectPalette.secondary.main};
+            }
+          }
+        `}
+      >
+        <a href={props.url}>{props.url_note}</a>
+      </Typography>
+    </React.Fragment>
+  );
+};
