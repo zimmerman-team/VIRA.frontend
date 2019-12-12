5846c4d8cc61bd7543cee2d61feee19464a253d1
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-12-12 10:04:19 +0100

    chore: made project compile again

--- src/app/modules/detail-modules/project-detail/mock.tsx
+++ src/app/modules/detail-modules/project-detail/mock.tsx
@@ -41,7 +41,7 @@ export const ProjectOutcomeCardMock: OutcomeCardParams[] = [
 ];
 
 export const DescriptionMock: DescriptionParams = {
-  description:
+  project_description:
     'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.',
 };
 
