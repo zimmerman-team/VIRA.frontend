f199e4a9fcbc6b9de8022575aa1ae58e6272d323
Jim Fung <jim@zimmermanzimmerman.nl>
2020-02-11 13:42:22 +0100

    feat: responsiveness

--- src/app/modules/detail-modules/report-detail/layout.tsx
+++ src/app/modules/detail-modules/report-detail/layout.tsx
@@ -1,6 +1,6 @@
 // global
 import React from 'react';
-import { Typography, Box, Grid } from '@material-ui/core';
+import { Typography, Box, Grid, Hidden } from '@material-ui/core';
 
 // absolute
 import { FieldDescription } from 'app/modules/report/sub-modules/indicator-verification/common/FieldDescription';
@@ -16,12 +16,16 @@ import uniqBy from 'lodash/uniqBy';
 
 // direct
 import 'styled-components/macro';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import graph1 from 'app/assets/images/dummy_graph1.png';
 
 export const ReportDetailLayout = (props: any) => (
   <React.Fragment>
     {/* ---------------------------------------------------------------------*/}
     {/* breadcrumbs */}
-    <Grid item lg={12}>
+    <Grid item xs={12} lg={12}>
       <BreadCrumbs
         currentLocation={props.report.title}
         previousLocations={[{ label: 'Reports', url: '/list/reports' }]}
@@ -30,141 +34,109 @@ export const ReportDetailLayout = (props: any) => (
 
     {/* ---------------------------------------------------------------------*/}
     {/* helper grid */}
-    <Grid item lg={6} />
+    <Hidden smDown>
+      <Grid item lg={6} />
+    </Hidden>
 
     {/* ---------------------------------------------------------------------*/}
     {/* title fragment */}
-    <Grid item container lg={12} direction="column">
+    <Grid item xs={12} lg={12}>
       <TitleFragment
-        note={props.report.date}
         title={props.report.title}
-        url_note={props.report.project.name}
         id={`Report ID: ${props.report.reportID}`}
+        note={props.report.date}
+        url_note={props.report.project.name}
         url={`/projects/${props.report.project.id}/detail`}
       />
     </Grid>
 
-    <Grid
-      item
-      container
-      xs={12}
-      lg={12}
-      alignItems="flex-start"
-      justify="flex-start"
-      alignContent="flex-start"
-    >
-      <Grid item xs={12} md={6} lg={6}>
-        <Grid item container lg={12}>
-          <Grid item lg={12}>
-            <FieldDescription text="Target beneficiaries" />
-          </Grid>
-          <Box width="100%" height="24px" />
-          <Grid item lg={12}>
-            <IntentTexFieldSingleLine
-              type="number"
-              min={0}
-              disabled
-              setValue={() => {}}
-              value={props.report.total_target_beneficiaries}
-              description="Total number: "
-            />
-          </Grid>
-        </Grid>
-        <Box height="40px" width="100%" />
-        <FieldDescription text="Of which the beneficiaries will likely include approximately" />
-      </Grid>
-      <Grid item xs={12} md={6} lg={6}>
-        <OutcomeCard title="Location" description={props.report.place} />
-      </Grid>
+    {/* ---------------------------------------------------------------------*/}
+    {/* Target beneficiaries */}
+    <Grid item xs={12} md={6} lg={6}>
+      <Card>
+        <CardHeader title="Target beneficiaries" />
+
+        <CardContent>
+          <IntentTexFieldSingleLine
+            type="number"
+            min={0}
+            disabled
+            setValue={() => {}}
+            value={props.report.total_target_beneficiaries}
+            description="Total number: "
+          />
+        </CardContent>
+      </Card>
     </Grid>
 
     {/* ---------------------------------------------------------------------*/}
-    {/* priority area chart */}
-    <Grid
-      item
-      container
-      xs={12}
-      lg={12}
-      alignItems="flex-start"
-      justify="flex-start"
-      alignContent="flex-start"
-    >
-      <Typography
-        variant="h5"
-        css={`
-          font-size: 20px;
-          font-weight: 600;
-        `}
-      >
-        Insinger Foundation policy priorities
-      </Typography>
-      <Box height="80px" width="100%" />
-      <Grid item lg={12}>
-        <HorizontalBarChart
-          maxValue={
-            get(props.report.barChartData[0], 'value1', 0) +
-            get(props.report.barChartData[0], 'value2', 0)
-          }
-          values={props.report.barChartData}
-          chartLegends={props.barChartLegends}
-          onChartLegendClick={props.onBarChartLegendClick}
-          colors={[
-            ProjectPalette.primary.main,
-            ...uniqBy(props.report.barChartData, 'value2Color').map(
-              (item: any) => item.value2Color
-            ),
-          ]}
-        />
-      </Grid>
+    {/* Location */}
+    <Grid item xs={12} md={6} lg={6}>
+      <OutcomeCard title="Location" description={props.report.place} />
+    </Grid>
+
+    {/* ---------------------------------------------------------------------*/}
+    {/* Insinger Foundation policy priorities */}
+    <Grid item xs={12} md={6} lg={12}>
+      <Card>
+        <CardHeader title="Insinger Foundation policy priorities" />
+        <Box height="20px" width="100%" />
+        <CardContent>
+          <HorizontalBarChart
+            maxValue={
+              get(props.report.barChartData[0], 'value1', 0) +
+              get(props.report.barChartData[0], 'value2', 0)
+            }
+            values={props.report.barChartData}
+            chartLegends={props.barChartLegends}
+            onChartLegendClick={props.onBarChartLegendClick}
+            colors={[
+              ProjectPalette.primary.main,
+              ...uniqBy(props.report.barChartData, 'value2Color').map(
+                (item: any) => item.value2Color
+              ),
+            ]}
+          />
+        </CardContent>
+      </Card>
     </Grid>
 
     <Box height="32px" width="100%" />
 
     {/* ---------------------------------------------------------------------*/}
     {/* cards */}
-    <Grid
-      item
-      container
-      xs={12}
-      lg={12}
-      spacing={2}
-      alignItems="flex-start"
-      justify="flex-start"
-      alignContent="flex-start"
-    >
-      <Grid item xs={12} md={6} lg={6}>
-        <OutcomeCard
-          title="Key outcomes"
-          description={props.report.key_outcomes}
-        />
-      </Grid>
-      <Grid item xs={12} md={6} lg={6}>
-        <OutcomeCard
-          title="Key implementation challenges"
-          description={props.report.key_implementation_challenges}
-        />
-      </Grid>
-      <Grid item xs={12} md={6} lg={6}>
-        <OutcomeCard
-          title="Monitor and report on the outcomes"
-          description={props.report.monitor_report_outcomes}
-        />
-      </Grid>
-      <Grid item xs={12} md={6} lg={6}>
-        <OutcomeCard
-          title="Other project outcomes and observations"
-          description={props.report.other_project_outcomes}
-        />
-      </Grid>
-      <Grid item xs={12} md={6} lg={6}>
-        <OutcomeCard title="Future plans" description={props.report.plans} />
-      </Grid>
-      <Grid item xs={12} md={6} lg={6}>
-        <OutcomeCard
-          title="Other comments"
-          description={props.report.other_comments}
-        />
-      </Grid>
+    <Grid item xs={12} md={6} lg={6}>
+      <OutcomeCard
+        title="Key outcomes"
+        description={props.report.key_outcomes}
+      />
+    </Grid>
+    <Grid item xs={12} md={6} lg={6}>
+      <OutcomeCard
+        title="Key implementation challenges"
+        description={props.report.key_implementation_challenges}
+      />
+    </Grid>
+    <Grid item xs={12} md={6} lg={6}>
+      <OutcomeCard
+        title="Monitor and report on the outcomes"
+        description={props.report.monitor_report_outcomes}
+      />
+    </Grid>
+    <Grid item xs={12} md={6} lg={6}>
+      <OutcomeCard
+        title="Other project outcomes and observations"
+        description={props.report.other_project_outcomes}
+      />
+    </Grid>
+    <Grid item xs={12} md={6} lg={6}>
+      <OutcomeCard title="Future plans" description={props.report.plans} />
+    </Grid>
+    <Grid item xs={12} md={6} lg={6}>
+      <OutcomeCard
+        title="Other comments"
+        description={props.report.other_comments}
+      />
     </Grid>
   </React.Fragment>
 );
