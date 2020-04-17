7a1650b43b840a2018e218f0d5de449ed23820e5
Stefanos <stefanos@zimmermanzimmerman.nl>
2020-04-17 15:40:24 +0300

    chore: remove console log

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -22,7 +22,6 @@ export function App() {
   const reduxLng = useStoreState(state => state.syncVariables.lng);
 
   React.useEffect(() => {
-    console.log(process.env);
     if (i18n.language !== reduxLng && reduxLng) {
       i18n.changeLanguage(reduxLng);
     }
