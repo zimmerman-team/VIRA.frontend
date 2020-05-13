b480f1c0d5f072652139939a5378dc99866e0469
Jim Fung <jim@zimmermanzimmerman.nl>
2020-05-13 21:50:40 +0200

    fix: arrow icon

--- src/app/components/inputs/select/index.tsx
+++ src/app/components/inputs/select/index.tsx
@@ -1,13 +1,56 @@
+// @ts-nocheck
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  withStyles,
+} from '@material-ui/core/styles';
 import { SelectItemModel } from 'app/components/inputs/select/model';
 import React from 'react';
+import InputBase from '@material-ui/core/InputBase';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import { css } from 'styled-components/macro';
+
+const BootstrapInput = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      'label + &': {
+        marginTop: theme.spacing(3),
+      },
+    },
+    icon: {
+      outline: '1px solid red',
+    },
+    iconFilled: {
+      outline: '1px solid red',
+      color: 'green',
+    },
+    input: {
+      borderRadius: 2,
+      position: 'relative',
+      backgroundColor: '#F0F3F7',
+      width: '338px',
+      //
+      // fontSize: 16,
+      padding: '10px 26px 10px 12px',
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+
+      '&:focus': {
+        borderRadius: 2,
+        borderColor: '#80bdff',
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+      },
+    },
+  })
+)(InputBase);
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,15 +63,16 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export function TeamSelect(props: any) {
   const classes = useStyles();
-  const inputLabel = React.useRef<HTMLLabelElement>(null);
-  const [labelWidth, setLabelWidth] = React.useState(150);
-  React.useEffect(() => {
+  // const inputLabel = React.useRef<HTMLLabelElement>(null);
+  // const [labelWidth, setLabelWidth] = React.useState(150);
+  /*  React.useEffect(() => {
     setLabelWidth(inputLabel.current!.offsetWidth);
-  }, []);
+  }, []);*/
 
+  // @ts-ignore
   return (
     <FormControl variant="outlined" className={classes.formControl}>
-      <InputLabel
+      {/*<InputLabel
         ref={inputLabel}
         id="select-outlined-label"
         css={`
@@ -36,13 +80,27 @@ export function TeamSelect(props: any) {
         `}
       >
         {props.title}
-      </InputLabel>
+      </InputLabel>*/}
+      <div
+        css={`
+          font-style: normal;
+          font-weight: 500;
+          font-size: 16px;
+          line-height: 24px;
+          margin-bottom: 15px;
+        `}
+      >
+        Select team
+      </div>
       <Select
         id="select-outlined"
-        labelWidth={labelWidth}
+        // labelWidth={labelWidth}
+        input={<BootstrapInput />}
         onChange={props.onChange}
         value={props.selectedItem}
         labelId="select-outlined-label"
+        variant="filled"
+        IconComponent={ExpandMoreIcon}
       >
         <MenuItem value="">
           <em>None</em>
