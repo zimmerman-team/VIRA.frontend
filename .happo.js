f63271e068a528208f7d6540669d623ca5ad5219
Jim Fung <jim@zimmermanzimmerman.nl>
2020-05-18 16:36:50 +0200

    Update .happo.js

--- .happo.js
+++ .happo.js
@@ -3,7 +3,7 @@ const { RemoteBrowserTarget } = require('happo.io');
 module.exports = {
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
-
+  project: 'insinger-frontend',
   targets: {
     'chrome-desktop': new RemoteBrowserTarget('chrome', {
       viewport: '1440x1024',
