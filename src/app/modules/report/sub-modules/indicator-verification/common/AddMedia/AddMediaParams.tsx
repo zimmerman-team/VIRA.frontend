b3e16f97bdf4fab19ededef045ef02035697aa14
Jim Fung <jim@zimmermanzimmerman.nl>
2020-06-09 17:18:58 +0200

    test: attribute refactor

--- src/app/modules/report/sub-modules/indicator-verification/common/AddMedia/AddMediaParams.tsx
+++ src/app/modules/report/sub-modules/indicator-verification/common/AddMedia/AddMediaParams.tsx
@@ -79,12 +79,13 @@ export const AddMediaNavContainer = (props: {
 export interface AddMediaButtonParams {
   text: string;
   onClick: Function;
+  testAttr: string;
 }
 
 export const AddMediaButton = (props: AddMediaButtonParams) => {
   return (
     <div
-      data-cy="add-media-button"
+      data-cy={props.testAttr}
       onClick={() => props.onClick()}
       css={`
         font-size: 14px;
