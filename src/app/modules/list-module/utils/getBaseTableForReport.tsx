8f7fc6b3eb3f4f36160b324588fad331040c514c
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2020-07-06 17:44:15 +0200

    chore: incremental commit

--- src/app/modules/list-module/utils/getBaseTableForReport.tsx
+++ src/app/modules/list-module/utils/getBaseTableForReport.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import React from 'react';
+import React, { useState } from 'react';
 
 import { LinkCellModule } from 'app/components/datadisplay/Table/common/LinkCell';
 import { TableModuleModel } from 'app/components/datadisplay/Table/model';
@@ -19,6 +19,22 @@ const options: MUIDataTableOptions = {
   selectableRows: false,
   viewColumns: false,
 };
+
+function formatDate(epoch: number) {
+  const date = new Date(epoch * 1000);
+  let day = date.getUTCDate().toString();
+  let month = (date.getUTCMonth() + 1).toString();
+  let year = date.getUTCFullYear().toString();
+  let formatted = '';
+  if (day.length === 1) {
+    day = `0${day}`;
+  }
+  if (month.length === 1) {
+    month = `0${month}`;
+  }
+  formatted = `${day}-${month}-${year}`;
+  return formatted;
+}
 
 export const getBaseTableForReport = (data: any): TableModuleModel => {
   const tableConfig = { ...ReportListMock, data: [] };
@@ -72,6 +88,43 @@ export const getBaseTableForReport = (data: any): TableModuleModel => {
         filter: true,
         filterType: 'custom',
         display: false,
+        // customFilterListOptions: {
+        //   render: epoch => {
+        //     let startDate = formatDate(epoch[0]);
+        //     let endDate = formatDate(epoch[1]);
+        //     if (startDate && endDate && dateFilterChecked) {
+        //       return [`Start Date: ${startDate}`, `End Date: ${endDate}`];
+        //     }
+        //     if (startDate && endDate && !dateFilterChecked) {
+        //       return `From: ${startDate}, To: ${endDate}`;
+        //     }
+        //     if (startDate) {
+        //       return `From: ${startDate}`;
+        //     }
+        //     if (endDate) {
+        //       return `To: ${endDate}`;
+        //     }
+        //     return [];
+        //   },
+        //   update: (filterList, filterPos, index) => {
+        //     console.log(
+        //       'customFilterListOnDelete: ',
+        //       filterList,
+        //       filterPos,
+        //       index
+        //     );
+
+        //     if (filterPos === 0) {
+        //       filterList[index].splice(filterPos, 1, '');
+        //     } else if (filterPos === 1) {
+        //       filterList[index].splice(filterPos, 1);
+        //     } else if (filterPos === -1) {
+        //       filterList[index] = [];
+        //     }
+
+        //     return filterList;
+        //   },
+        // },
         filterOptions: {
           names: [],
           logic(date, filters) {
@@ -89,6 +142,7 @@ export const getBaseTableForReport = (data: any): TableModuleModel => {
               filterList[index][0] = Date.parse(dateRange.startDate) / 1000;
               filterList[index][1] = Date.parse(dateRange.endDate) / 1000;
               onChange(filterList[index], index, column);
+              console.log('filterlist', filterList[index][0]);
             }
 
             return (
