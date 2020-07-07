2f307849ae3bcb261ec5b67f7c6429f752fd9db8
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2020-07-07 15:57:35 +0200

    test: refactoring of almost all e2e tests
    [IN-531]

--- cypress/integration/edit-report.spec.js
+++ cypress/integration/edit-report.spec.js
@@ -1,6 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('edit report page', () => {
+  it('test report overview page', () => {
+    // authenticate
+    cy.auth();
+    // goto page
+    // cy.visit('/list/2');
+    cy.findByTestId('sidebar-item-3').click();
+
+    cy.listTabs();
+
+    cy.get('body').happoScreenshot({
+      component: 'Report overview',
+      variant: 'base',
+    });
+    // cy.get('body').happoScreenshot();
+  });
   it('test edit report page', () => {
     // authenticate
     cy.auth();
--- cypress/not-used/grantee-overview.spec.js
+++ cypress/not-used/grantee-overview.spec.js
@@ -1,10 +1,11 @@
 /// <reference types="Cypress" />
+// DONE
 describe('grantee overview page', () => {
   it('test grantee overview page', () => {
     // authenticate
     cy.auth();
     // goto page
-    // cy.visit('/list/1');
+    cy.visit('/list/1');
     // cy.get([data-cy=sidebar-item-2] > .MuiButtonBase-root
 
     cy.findByTestId('sidebar-item-2').click();
