382149e1694a33798fd0f64854a4f549c2228c3a
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2020-07-10 13:48:50 +0200

    refactor: simplify conditional logic
    [IN-523]

--- src/app/modules/report/utils/validateOutcomeFields.ts
+++ src/app/modules/report/utils/validateOutcomeFields.ts
@@ -2,9 +2,5 @@ export function validateOutcomeFields(title: string, country: string) {
   if (title === '') {
     return false;
   }
-  /* todo: can this be simplified? */
-  if (country === '') {
-    return false;
-  }
-  return true;
+  return country !== '';
 }
