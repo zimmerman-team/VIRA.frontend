e34aa04a544be2de4ac5770e9890e878a4fbcc56
Jim Fung <jim@zimmermanzimmerman.nl>
2020-09-15 10:15:44 +0200

    style: prettier

--- cypress/integration/user-management.spec.js
+++ cypress/integration/user-management.spec.js
@@ -5,52 +5,25 @@ describe('create and delete admin user', () => {
   it('go to users overview', () => {
     cy.auth();
 
-    cy.findByTestId('usercard-button')
-      .should('exist')
-      .click();
-    cy.findByTestId('usercard-manage-teams-button')
-      .should('exist')
-      .click();
-    cy.findByTestId('user_management.general.users')
-      .should('exist')
-      .click();
+    cy.findByTestId('usercard-button').should('exist').click();
+    cy.findByTestId('usercard-manage-teams-button').should('exist').click();
+    cy.findByTestId('user_management.general.users').should('exist').click();
   });
 
   it('create new admin user', () => {
-    cy.findByTestId('admin-add-button')
-      .should('exist')
-      .click();
-    cy.findByTestId('FirstName')
-      .should('exist')
-      .click()
-      .type('Cypress');
-    cy.findByTestId('LastName')
-      .should('exist')
-      .click()
-      .type(id);
-    cy.findByTestId('Email')
-      .should('exist')
-      .click()
-      .type(`${id}@user.test`);
-    cy.get('#select-outlined')
-      .should('exist')
-      .click();
+    cy.findByTestId('admin-add-button').should('exist').click();
+    cy.findByTestId('FirstName').should('exist').click().type('Cypress');
+    cy.findByTestId('LastName').should('exist').click().type(id);
+    cy.findByTestId('Email').should('exist').click().type(`${id}@user.test`);
+    cy.get('#select-outlined').should('exist').click();
     cy.get('[data-value="e0d70f9c-30e3-476e-b25f-a663daa1acb9"]')
       .should('exist')
       .click();
-    cy.findByTestId('contained-button')
-      .should('exist')
-      .click();
+    cy.findByTestId('contained-button').should('exist').click();
     cy.wait(1000);
-    cy.findByTestId('usercard-button')
-      .should('exist')
-      .click();
-    cy.findByTestId('usercard-manage-teams-button')
-      .should('exist')
-      .click();
-    cy.findByTestId('user_management.general.users')
-      .should('exist')
-      .click();
+    cy.findByTestId('usercard-button').should('exist').click();
+    cy.findByTestId('usercard-manage-teams-button').should('exist').click();
+    cy.findByTestId('user_management.general.users').should('exist').click();
   });
 
   /*  it('adjust user auth permissions', () => {
@@ -82,12 +55,8 @@ describe('create and delete admin user', () => {
   });*/
 
   it('delete user', () => {
-    cy.findByTestId(`delete-Cypress ${id}`)
-      .should('exist')
-      .click();
-    cy.findByText('Delete')
-      .should('exist')
-      .click();
+    cy.findByTestId(`delete-Cypress ${id}`).should('exist').click();
+    cy.findByText('Delete').should('exist').click();
     cy.wait(1000);
   });
 });
